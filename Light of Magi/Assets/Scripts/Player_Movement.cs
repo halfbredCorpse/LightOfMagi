03151d2fb1456bcb50b3ea2167a54348a08f931b
Nathanael Aguas <leesauga@gmail.com>
2017-11-06 16:34:06 +0800

    Initial Upload

---
+++ Light of Magi/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public Transform player;
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
+        else if (Input.GetKey(KeyCode.DownArrow))
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, -speed);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, 0);
+        else
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+    }
+}
