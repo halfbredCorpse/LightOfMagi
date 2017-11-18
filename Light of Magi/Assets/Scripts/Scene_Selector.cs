cc899d40da8a8d34f560e4e20de0a8091980f4f5
Nathanael Aguas <leesauga@gmail.com>
2017-11-19 01:09:13 +0800

    Latest build from Lim branch

---
+++ Light of Magi/Assets/Scripts/Scene_Selector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Selector : MonoBehaviour {
+
+    // Use this for initialization
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+        Debug.Log(scene);
+    }
+}
