ac4f7d55c669313a2db0caa9f1e59f4de149e3ed
Jasmin Lim <jrlim13@yahoo.com>
2017-11-19 08:49:29 +0800

    Touch Movement
    Works, but the character does not stop the movement. Check "Player_Movement" Script.

---
+++ Light of Magi/Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Movement : MonoBehaviour {
+
+    float dirX;
+    float dirY;
+    public float speed = 0.7f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        dirX = CrossPlatformInputManager.GetAxis("Horizontal");
+        dirY = CrossPlatformInputManager.GetAxis("Vertical");
+
+        transform.position = new Vector2(transform.position.x + dirX, transform.position.y + dirY);
+	}
+}
--- Light of Magi/Assets/Scripts/Player_Movement.cs
+++ Light of Magi/Assets/Scripts/Player_Movement.cs
@@ -2,80 +2,131 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class Player_Movement : MonoBehaviour
 {
+    private static ILogger logger = Debug.unityLogger;
     public Transform player;
     public float speed;
     private Animator animator;
     private KeyCode recKey;
+    float dirX;
+    float dirY;
+    bool forward, backward, right, left;
 
     // Use this for initialization
     void Start()
     {
         animator = this.GetComponent<Animator>();
+
+        forward = false;
+        backward = false;
+        right = false;
+        left = false;
+    }
+
+    public void moveForward()
+    {
+        animator.Play("Magi 1_Back_ Walking_Anim");
+        forward = true;
+        backward = false;
+        right = false;
+        left = false;
+    }
+
+    public void moveBackward()
+    {
+        animator.Play("Magi 1_Front_Walking_Anim");
+        forward = false;
+        backward = true;
+        right = false;
+        left = false;
+    }
+
+    public void moveRight()
+    {
+        animator.Play("Magi 1_Right_Walking_Anim");
+        forward = false;
+        backward = false;
+        right = true;
+        left = false;
+    }
+
+    public void moveLeft()
+    {
+        animator.Play("Magi 1_Left_Walking_Anim");
+        forward = false;
+        backward = false;
+        right = false;
+        left = true;
+    }
+
+    public void StopMovement()
+    {
+        if(Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetTouch(0).phase == TouchPhase.Canceled)
+        {
+            if (recKey == KeyCode.UpArrow)
+            {
+                animator.Play("Magi 1_Back_ Idle_Anim");
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            }
+            else if (recKey == KeyCode.DownArrow)
+            {
+                animator.Play("Magi 1_Front_Idle_Anim");
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            }
+            else if (recKey == KeyCode.RightArrow)
+            {
+                animator.Play("Magi 1_Right_Idle_Anim");
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            }
+            else if (recKey == KeyCode.LeftArrow)
+            {
+                animator.Play("Magi 1_Left_Idle_Anim");
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
-    {
-        /*
-         * Direction = 0; Front/Idle
-         * Direction = 1; Back/Idle
-         * Direction = 2; Right/Idle
-         * Direction = 3; Left/Idle
-         * Direction = 4; Front/Walking
-         * Direction = 5; Back/Walking
-         * Direction = 6; Right/Walking
-         * Direction = 7; Left/Walking
-         */
+    {  
+        //if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        //{
+            var vertical = CrossPlatformInputManager.GetAxis("Vertical");
+            var horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
 
-        var vertical = Input.GetAxis("Vertical");
-        var horizontal = Input.GetAxis("Horizontal");
+            if (forward)
+            {
+                recKey = KeyCode.UpArrow;
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
+                logger.Log("Vertical", vertical);
+                logger.Log("Horizontal", horizontal);
+            }
+            else if (backward)
+            {
+                recKey = KeyCode.DownArrow;
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0, -speed);
+                logger.Log("Vertical", vertical);
+                logger.Log("Horizontal", horizontal);
+            }
+            else if (right)
+            {
+                recKey = KeyCode.RightArrow;
+                GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+                logger.Log("Vertical", vertical);
+                logger.Log("Horizontal", horizontal);
+            }
+            else if (left)
+            {
+                recKey = KeyCode.LeftArrow;
+                GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, 0);
+                logger.Log("Vertical", vertical);
+                logger.Log("Horizontal", horizontal);
+            }
 
-        if (vertical > 0)
-        {
-            recKey = KeyCode.UpArrow;
-            animator.Play("Magi 1_Back_ Walking_Anim");
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
-        }
-        else if (vertical < 0)
-        {
-            recKey = KeyCode.DownArrow;
-            animator.Play("Magi 1_Front_Walking_Anim");
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, -speed);
-        }
-        else if (horizontal > 0)
-        {
-            recKey = KeyCode.RightArrow;
-            animator.Play("Magi 1_Right_Walking_Anim");
-            GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
-        }
-        else if (horizontal < 0)
-        {
-            recKey = KeyCode.LeftArrow;
-            animator.Play("Magi 1_Left_Walking_Anim");
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, 0);
-        }
-        else if (recKey == KeyCode.UpArrow)
-        {
-            animator.Play("Magi 1_Back_ Idle_Anim");
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-        }
-        else if (recKey == KeyCode.DownArrow)
-        {
-            animator.Play("Magi 1_Front_Idle_Anim");
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-        }
-        else if (recKey == KeyCode.RightArrow)
-        {
-            animator.Play("Magi 1_Right_Idle_Anim");
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-        }
-        else if (recKey == KeyCode.LeftArrow)
-        {
-            animator.Play("Magi 1_Left_Idle_Anim");
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-        }
+            //StopMovement();
+        //}
     }
 }
