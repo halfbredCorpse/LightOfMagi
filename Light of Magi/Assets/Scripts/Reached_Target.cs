73b2e73370c7e2c9ed6246e5dc3a40417d30e50a
Nathanael Aguas <leesauga@gmail.com>
2017-11-22 08:20:25 +0800

    Added Incomplete Suffocation

---
+++ Light of Magi/Assets/Scripts/Reached_Target.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reached_Target : MonoBehaviour {
+
+    GameObject target;
+    GameObject secondCanvas;
+
+	// Use this for initialization
+	void Start () {
+        secondCanvas = GameObject.FindGameObjectWithTag("SecondCanvas");
+        secondCanvas.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (target != null)
+        {
+            if (Vector2.Distance(transform.position, target.transform.position) <= 0.1)
+            {
+                Debug.Log("Enter");
+                secondCanvas.SetActive(true);
+                target = null;
+            }
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Goal" || collision.gameObject.tag == "Magi")
+        {
+            target = collision.gameObject;
+            Physics2D.IgnoreCollision(target.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        }
+    }
+}
